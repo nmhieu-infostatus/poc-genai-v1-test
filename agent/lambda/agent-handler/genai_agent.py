da8a6a03e0eecdc6c69622447eeb90ac43e5ea20
nmhieu-infostatus <hieu.nguyen@infostatus.com.au>
2024-06-20 01:04:18 +0700

    modify agent handler: remove FSI custom, remove Loan... intent

--- agent/lambda/agent-handler/genai_agent.py
+++ agent/lambda/agent-handler/genai_agent.py
@@ -4,23 +4,29 @@ from langchain.agents import AgentExecutor
 from tools import Tools
 from datetime import datetime
 
-class FSIAgent:
-    
+
+class GenAIAgent:
+    """
+    Create an AI agent that can be used to answer questions or perform tasks using a set of predefined tools.
+    tools_instance.kendra_search is a custom tool that the agent can use to retrieve information.
+    """
+
     def __init__(self, llm, memory) -> None:
-        self.ai_prefix = "Assistant"
-        self.human_prefix = "Human"
+        self.ai_prefix = "Assistant" # call the genai agent "Assistant"
+        self.human_prefix = "Human" # call the user of chatbot "Human"
         self.llm = llm
         self.memory = memory
         self.tools_instance = Tools()  # Define tools_instance here
         self.agent = self.create_agent()
 
     def create_agent(self):
-        # Initialize the agent with only the AnyCompany tool
-        anycompany_tool = Tool(name="AnyCompany", func=self.tools_instance.kendra_search, description="Use this tool to answer questions about AnyCompany.")
-        
-        fsi_agent = ConversationalAgent.from_llm_and_tools(
+        # Initialize the agent with only the custom tool class from tools.py
+        genai_tool = Tool(name="GenAI", func=self.tools_instance.kendra_search,
+                               description="Use this tool to answer questions about your projects.")
+
+        genai_agent = ConversationalAgent.from_llm_and_tools(
             llm=self.llm,
-            tools=[anycompany_tool],
+            tools=[genai_tool],
             ai_prefix=self.ai_prefix,
             human_prefix=self.human_prefix,
             verbose=True,
@@ -29,18 +35,18 @@ class FSIAgent:
         )
 
         agent_executor = AgentExecutor.from_agent_and_tools(
-            agent=fsi_agent,
-            tools=[anycompany_tool],
+            agent=genai_agent,
+            tools=[genai_tool],
             verbose=True,
             memory=self.memory,
             return_source_documents=True,
             return_intermediate_steps=True
         )
-        
+
         return agent_executor
 
     def run(self, input):
-        print("Running FSI Agent with input: " + str(input))
+        print("Running GenAI Agent with input: " + str(input))
         try:
             response = self.tools_instance.kendra_search(input)
         except ValueError as e:
